291cd230df8b6683e92a108ee47d65e0d8db0f52
Waldemar Teichreb <waldi.teichreb@gmail.com>
2022-06-22 00:39:22 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter';
+  count = 0;
+  increment = () => {
+    this.count++;
+    console.log(this.count);
+  };
+  decrement = () => {
+    this.count--;
+    console.log(this.count);
+  };
 }
